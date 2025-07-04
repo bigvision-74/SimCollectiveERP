13d66540d0edfe2cdfed8d86e10fbfe075f4d24d
Ramanpreet Kaur <preetraman9781@gmail.com>
2025-07-04 09:16:21 +0530

    new changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,77 @@
-import ScrollToTop from "@/components/Base/ScrollToTop";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./stores/store";
 import Router from "./router";
 import "./assets/css/app.css";
+import "./i18n";
+import ScrollToTop from "@/components/Base/ScrollToTop";
+// import FaviconUpdater from "./pages/SettingsData";
+// import { useInactivityTracker } from "./actions/userActive";
+import { checkLoginDuration } from "./actions/authAction";
+// import ErrorBoundary from './Errorboundary';
+import { HelmetProvider } from "react-helmet-async";
+
+// import { UploadProvider } from "./UploadContext";
+// import UploadStatusIndicator from "./UploadStatusIndicator";
+// import { UploadProvider } from "@/components/UploadContext1/UploadContext";
+// import UploadStatusIndicator from "@/components/UploadStatusIndicator1/UploadStatusIndicator";
+// import { UploadProvider } from "@/components/UploadContext";
+// import UploadStatus from "@/components/UploadStatus"
+const CssPreloader = ({ children }: { children: React.ReactNode }) => {
+  const [stylesLoaded, setStylesLoaded] = React.useState(false);
+
+  useEffect(() => {
+    const styleSheets = Array.from(document.styleSheets);
+    const appCssLoaded = styleSheets.some(
+      (sheet) =>
+        sheet.href?.includes("app.css") || sheet.href?.includes("chunk.css")
+    );
+
+    if (appCssLoaded) {
+      setStylesLoaded(true);
+      return;
+    }
+
+    const timer = setTimeout(() => setStylesLoaded(true), 300);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return stylesLoaded ? <>{children}</> : null;
+};
+
+// const App = () => {
+//   useInactivityTracker();
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       checkLoginDuration();
+//     }, 60000);
+
+//     return () => clearInterval(intervalId);
+//   }, []);
+
+//   return (
+//     <UploadProvider>
+//       <CssPreloader>
+//         <FaviconUpdater />
+//         <ScrollToTop />
+//         <Router />
+//         <UploadStatus />
+//       </CssPreloader>
+//     </UploadProvider>
+//   );
+// };
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-    <ScrollToTop />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <HelmetProvider>
+          {/* <App /> */}
+        </HelmetProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
--- server/server.js
+++ server/server.js
@@ -3,16 +3,35 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const path = require("path");
 const userRoutes = require("./routes/userRoutes");
+const json11 = require("./i18n/en_uk.json");
+const json22 = require("./i18n/es.json");
+const json33 = require("./i18n/fr.json");
+const json44 = require("./i18n/en.json");
+const helmet = require("helmet");
+const cors = require("cors");
 // Middleware (optional)
+app.use(cors());
 app.use(express.json());
-
+require("dotenv").config();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-user-name"],
+  })
+);
+app.use(helmet({
+  hidePoweredBy: { setTo: 'null' },
+}));
 // Test route
 app.get('/', (req, res) => {
   res.send('Hello from Express server!');
 });
 
 app.use(userRoutes);
+app.use("/i18n", express.static(path.join(__dirname, "i18n")));
 
 // Start server
 app.listen(PORT, () => {
