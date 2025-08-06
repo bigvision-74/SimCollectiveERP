92667bf09833f0adf74f2283bcc2b6eff884f2b8
srishtyaray <srishtyarya16@gmail.com>
2025-08-06 11:40:57 +0530

    changes

--- client/src/components/Payment/index.tsx
+++ client/src/components/Payment/index.tsx
@@ -19,7 +19,6 @@ interface PlanDetails {
   title: string;
   price: string;
   duration: string;
-  // type: "Subscription" | "Perpetual License";
 }
 
 interface FormData {
@@ -477,28 +476,13 @@ const PaymentInformation: React.FC<PaymentInformationProps> = ({
               onClick={handleSubmit}
             >
               {isSubmitting ? (
-                <div className="flex items-center justify-center">
-                  <svg
-                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                  {t("Processing...")}
-                </div>
+                  <div className="flex items-center justify-center gap-2">
+                    <div className="loader">
+                      <div className="dot"></div>
+                      <div className="dot"></div>
+                      <div className="dot"></div>
+                    </div>
+                  </div>
               ) : (
                 `${t("Complete Payment")} - ${plan.price}`
               )}
