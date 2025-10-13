37892892c782ab494a4157bb4ab544520b587f7c
Ramanpreet Kaur <preetraman9781@gmail.com>
2025-10-13 10:12:22 +0530

    minor change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -91,7 +91,7 @@ const App = () => {
           <ScrollToTop />
 
           <Router />
-          <Version />
+          {/* <Version /> */}
           <UploadStatus />
         </AppPreloader>
       {/* )} */}
