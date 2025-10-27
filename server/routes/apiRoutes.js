f08afef7b8039eb2fa5cf2ffe6d7b7f4faf491fd
Ankit Dhiman <ankitphp.mxr@gmail.com>
2025-10-27 12:22:23 +0530

    summary api

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -4,12 +4,15 @@ const multer = require("multer");
 const upload = multer();
 
 const authenticate = require("../Authentication/auth");
-const { Login, sendOtpApi, verifyApi, getAllPatientsApi, getVirtualSessionByUserIdApi } = require("../controllers/apiController");
+const { Login, sendOtpApi, verifyApi, getAllPatientsApi, getVirtualSessionByUserIdApi, getPatientSummaryByIdApi } = require("../controllers/apiController");
 
 router.post("/login", Login);
 router.post("/sendOtpApi", sendOtpApi);
 router.post("/verifyApi", verifyApi);
 router.get("/getAllPatientsApi", getAllPatientsApi);
 router.post("/getVirtualSessionByUserIdApi", getVirtualSessionByUserIdApi);
+// router.get("/getPatientSummaryByIdApi/:patientId", getPatientSummaryByIdApi);
+router.get("/getPatientSummaryByIdApi", getPatientSummaryByIdApi);
+
 
 module.exports = router;
