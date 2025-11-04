2a2a54d190dd7b1847ccd1a6478674a7bdd9a9c2
Ankit Dhiman <ankitphp.mxr@gmail.com>
2025-11-04 12:44:29 +0530

    update controller

--- server/websocket.js
+++ server/websocket.js
@@ -3,7 +3,7 @@ const Knex = require("knex");
 const { name } = require("ejs");
 const knexConfig = require("./knexfile").development;
 const knex = Knex(knexConfig);
-const { initMediaSocketClient } = require("./appsocket");
+// const { initMediaSocketClient } = require("./appsocket");
 let io;
 
 const initWebSocket = (server) => {
@@ -23,7 +23,7 @@ const initWebSocket = (server) => {
     },
   });
 
-  initMediaSocketClient(io);
+  // initMediaSocketClient(io);
 
   io.use(async (socket, next) => {
     console.log(`[AUTH] New connection attempt from IP: ${socket.handshake.address}`);
