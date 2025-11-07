e7b760a0ad45bd2d528fa5b0fc37c83457e4453b
Ramanpreet Kaur <preetraman9781@gmail.com>
2025-11-07 11:35:28 +0530

    firebase notification

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -64,7 +64,7 @@ exports.createSession = async (req, res) => {
     };
 
     try {
-      const response = await secondaryApp.messaging().sendMulticast(message);
+      const response = await secondaryApp.messaging().sendEachForMulticast(message);
 
       console.log(
         `✅ Notifications sent. Success: ${response.successCount}, Failure: ${response.failureCount}`
